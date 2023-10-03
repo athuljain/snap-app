d0709b4d6fa947c76fb19aaa730e7fdf3ba4e4a5
athuljain <athuljainkj2@gmail.com>
2023-10-03 10:49:09 +0530

    DataJsx commit

--- src/Components/Datas.jsx
+++ src/Components/Datas.jsx
@@ -1,27 +1,27 @@
 export const JsonDatas = [
   {
-    description: "Mayanadhi movie - mathan , appu",
-    image:
+    "description": "Mayanadhi movie - mathan , appu",
+    "image":
       "https://i.pinimg.com/564x/af/c2/c2/afc2c209ed02f028b9c064ea09a18bbc.jpg",
   },
   {
-    description: "Illusrtaion",
-    image:
+    "description": "Illusrtaion",
+    "image":
       "https://i.pinimg.com/236x/e9/1b/86/e91b861de5f2e2cab57833b3ff5ff3bb.jpg",
   },
   {
-    description: "Beautiful scenery",
-    image:
+    "description": "Beautiful scenery",
+    "image":
       "https://i.pinimg.com/236x/d8/71/25/d871257b628eef13bceed5504f4fade2.jpg",
   },
   {
-    description: "Illusrtaion",
-    image:
+    "description": "Illusrtaion",
+    "image":
       "https://i.pinimg.com/236x/9b/7e/0a/9b7e0ad14647458ed374eba48b98f81f.jpg",
   },
   {
-    description: "Illusrtaion",
-    image:
+    "description": "Illusrtaion",
+    "image":
       "https://i.pinimg.com/236x/08/bd/00/08bd005bd19cdf15b0fdc2bd11b5fa8c.jpg",
   },
 ];
