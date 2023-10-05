1c08dd892ac6557c82451dd6fc77c4d903bc0743
athuljain <athuljainkj2@gmail.com>
2023-10-05 10:36:24 +0530

    shoesBrand commit

--- src/Components/Shoes.jsx
+++ src/Components/Shoes.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { myContext } from "../Context"
 import { Link } from "react-router-dom";
+import './Shoes.css'
 
 
 
@@ -12,20 +13,21 @@ export default function Shoes(){
     console.log("Shoes Products",shoes);
 
     return(
-        <div>
+        <div className="Container">
             <>
             <Link to={'/nike'}>Nike Shoes... </Link>
             <Link to={'/adidas'}>Adidas Shoes...</Link>
             </>
             <h1>Shoes Products ...</h1>
-            
+            <div className="container">
             {shoes.map((data,index)=>
-            <>
+            <div >
             <img  src={data.img} alt="img"></img>
             <h4>{data.name}</h4>
             <h5>{data.brand}</h5>
-            </>
+            </div>
             )}
+            </div>
 
         </div>
     )
@@ -43,15 +45,17 @@ export function Nike(){
       );
       console.log("Nike Products...",NikeShoes);
     return(
-        <div>
-<h1>Nike Shoes</h1>
+        <div className="Container">
+<h1>Nike Shoes...</h1>
+<div className="container">
 {NikeShoes.map((data)=>
-<>
+    <div >
 <img src={data.img} alt="img" ></img>
 <h1>{data.brand}</h1>
-</>
+</div>
 
 )}
+</div>
         </div>
     )
 }
@@ -66,13 +70,16 @@ export function Adidas(){
     console.log("Adidas Product", AdidasProduct);
 
     return(
-        <div>
+        <div className="Container">
+            <h1>Adidas Shoes...</h1>
+             <div className="container">
                 {AdidasProduct.map((data,index)=>
-                <>
+                <div >
                 <img src={data.img} alt="img" ></img>
                 <h1>{data.name}</h1>
-                </>
+                </div>
                 )}
+                </div>
         </div>
     )
 }
