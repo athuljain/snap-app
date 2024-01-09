67787d77a7e1c047da2127d531a6f3082ea4d56e
athuljain <athuljainkj2@gmail.com>
2024-01-09 12:30:14 +0530

    admin add product

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -1,26 +1,64 @@
-import { useContext } from "react"
-import { myContext } from "../Context"
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { myContext } from "../Context";
 
-export default function Admin(){
 
-    const {shoes, setShoes}=useContext(myContext)
+export default function Admin() {
+  const { shoes, setShoes } = useContext(myContext);
+  const [newProduct, setNewProduct] = useState({ name: "", brand: "", img: "" });
+  const [showForm, setShowForm] = useState(false);
 
-    console.log(shoes);
-    return(
-        <div>
-   <div className="container">
-          {shoes.map((data) => (
-            <div>
-              
-                <img src={data.img} alt="img"></img>
-                <h1>{data.name}</h1>
-                <h3>Brand : {data.brand}</h3>
-              
-             
-            </div>
-          ))}
-        </div>
-        </div>
-    )
+  const addProduct = () => {
+    setShoes((prevShoes) => [...prevShoes, newProduct]);
+    setNewProduct({ name: "", brand: "", img: "" });
+    setShowForm(false);
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <button onClick={() => setShowForm(true)}>Add Product</button>
+        {showForm && (
+          <form>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={newProduct.name}
+                onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+              />
+            </label>
+            <label>
+              Brand:
+              <input
+                type="text"
+                name="brand"
+                value={newProduct.brand}
+                onChange={(e) => setNewProduct({ ...newProduct, brand: e.target.value })}
+              />
+            </label>
+            <label>
+              Image URL:
+              <input
+                type="text"
+                name="img"
+                value={newProduct.img}
+                onChange={(e) => setNewProduct({ ...newProduct, img: e.target.value })}
+              />
+            </label>
+            <button type="button" onClick={addProduct}>
+              Add
+            </button>
+          </form>
+        )}
+        {shoes.map((data, index) => (
+          <div key={index}>
+            <img src={data.img} alt="img" />
+            <h1>{data.name}</h1>
+            <h3>Brand: {data.brand}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
