7b118d1032b8637d21077bc8df69c2f4e57f130b
athuljain <athuljainkj2@gmail.com>
2024-02-19 15:09:29 +0530

    setup data

--- src/Components/Datas.jsx
+++ src/Components/Datas.jsx
@@ -4,30 +4,35 @@ export const JsonDatas = [
     description: "Mayanadhi movie - mathan , appu",
     image:
       "https://i.pinimg.com/564x/af/c2/c2/afc2c209ed02f028b9c064ea09a18bbc.jpg",
+      price:100
   },
   {
     id: 2,
     description: "Illustration",
     image:
       "https://i.pinimg.com/236x/e9/1b/86/e91b861de5f2e2cab57833b3ff5ff3bb.jpg",
+      price:100
   },
   {
     id: 3,
     description: "Beautiful scenery",
     image:
       "https://i.pinimg.com/236x/d8/71/25/d871257b628eef13bceed5504f4fade2.jpg",
+      price:100
   },
   {
     id: 4,
     description: "Illustration",
     image:
       "https://i.pinimg.com/236x/9b/7e/0a/9b7e0ad14647458ed374eba48b98f81f.jpg",
+      price:100
   },
   {
     id: 5,
     description: "Illustration",
     image:
       "https://i.pinimg.com/236x/08/bd/00/08bd005bd19cdf15b0fdc2bd11b5fa8c.jpg",
+      price:100
   },
 ];
 
